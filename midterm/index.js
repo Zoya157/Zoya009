a5e87a4e7b79a1d396c28ccdc04d87cc2295e0bb
Zoya157 <115418544+Zoya157@users.noreply.github.com>
2023-04-17 13:48:58 +0500

    MIDTERM
    FA20-BCS-009

---
+++ midterm/index.js
@@ -0,0 +1,10 @@
+
+
+  <script>
+    window.onload = function() {
+      const logo = document.getElementById("logo");
+      logo.addEventListener("click", function() {
+        alert("FA20-BCS-009");
+      });
+    }
+  </script>
